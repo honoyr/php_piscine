210689934ccad915a9313684f7909fe1c1a192aa
Denys Gonor <dgonor@e1z2r3p39.42.us.org>
2019-01-10 23:50:57 -0800

    Thu Jan 10 23:50:57 PST 2019

--- day02/ex04/photos.php
+++ day02/ex04/photos.php
@@ -12,7 +12,7 @@
 		preg_match_all('/<img[^>]+>/',$html, $result); 
 		$i = 0;
         $img = array();
-        print_r($result);
+        // print_r($result);
 		foreach ($result[0] as $line)
 		{
 			$img[$i] = trim(preg_replace("( +)", " ", preg_replace("(\n+)", " ", $line)));
@@ -20,25 +20,38 @@
 			$i++;
         }
         // print($line). "\n";
-        print_r($img);
+        // print_r($img);
 		if(!is_dir($name)) {
 			mkdir($name, 0777, true);
         }
         // file_put_contents($name . "/" .$tempname, $tempimg);
 		foreach ($img as $key ) {
-			if (strlen($key[0][0]) > 1 && !strstr($key[0][0], "//")) {
+			if (strlen($key[0][0]) > 1) {
 				$key[1][0] = preg_replace("(\")", "", $key[1][0]);
 				$tempname = strrchr($key[1][0], "/");
 				$tempname = mb_substr($tempname, 1);
-				if ($key[1][0][0] != '/'){
-					$key[1][0] = $argv[1] . "/" . $key[1][0];
-				}
-				else {
-					$key[1][0] = $argv[1] . $key[1][0];
-				}
-				if (get_http_response_code($key[0][0]) == 200) {
+				// if ($key[1][0][0] != '/'){
+				// 	$key[1][0] = $argv[1] . "/" . $key[1][0];
+                // }
+				// else {
+				// 	$key[1][0] = $argv[1] . $key[1][0];
+                // }
+                print_r($key);
+				if (get_http_response_code($key[0][0])) {
 					$tempimg = file_get_contents($key[0][0]);
-				}
+                }
+                print_r($tempimg). "\n";
+                print_r($tempname). "\n";
+                print_r($name). "\n";
+
+
+
+
+
+
+
+
+
 				if (strlen($tempname) > 0) {
 					file_put_contents($name . "/" .$tempname, $tempimg);
 				}
