beb57704fc223654d911d9ebf039d950869cdc8b
Denys Gonor <dgonor@e1z3r3p22.42.us.org>
2019-01-12 13:58:08 -0800

    Sat Jan 12 13:58:08 PST 2019

--- day03/ex00/D03.php
+++ day03/ex00/D03.php
@@ -1,3 +1,19 @@
 #!/usr/bin/php
 <?php
 ?>
+
+
+<?php
+if ($_GET[login] && $_GET[passwd] && $_GET[submit] === "OK")
+{
+    setcookie($_GET[login], $_GET[passwd]);
+}
+if ($_GET[action] === "get")
+{
+    if ($_COOKIE[$_GET[login]])
+        echo $_COOKIE[$_GET[login]] . PHP_EOL . $_COOKIE[$_GET[passwd]] . PHP_EOL;
+}
+if ($_GET[action] === "del")
+    setcookie($_GET[name], "", time() - 3600);
+?>
+
