0e4336c8fce69a690b8c3229b7bf7edcedf4cd5d
Denys Gonor <dgonor@e1z3r1p19.42.us.org>
2019-01-15 12:55:26 -0800

    Tue Jan 15 12:55:26 PST 2019

--- day05/ex11/ex11.sql
+++ day05/ex11/ex11.sql
@@ -1,2 +1,25 @@
 SELECT last_name AS 'NAME', first_name FROM user_card;
 
+
+
+SELECT last_name AS 'NAME', first_name FROM user_card
+INNER JOIN mem ON subscription.price = mem.price
+INNER JOIN member ON user_card.last_name = member.last_name;
+
+
+SELECT last_name AS 'NAME', first_name FROM user_card
+INNER JOIN member ON member.id_user_card = user_card.id_user
+INNER JOIN subscription ON subscription.id_sub = member.id_sub;
+
+SELECT user_card.last_name AS 'NAME', user_card.first_name, subscription.price FROM user_card
+INNER JOIN member ON member.id_user_card = user_card.id_user
+INNER JOIN subscription ON subscription.id_sub = member.id_sub;
+
+SELECT user_card.last_name AS 'NAME', user_card.first_name, subscription.price FROM user_card
+INNER JOIN member ON member.id_user_card = user_card.id_user
+INNER JOIN subscription ON subscription.id_sub = member.id_sub;
+
+SELECT user_card.last_name AS 'NAME', user_card.first_name, subscription.price FROM user_card
+INNER JOIN member ON member.id_user_card = user_card.id_user
+INNER JOIN subscription ON subscription.id_sub = member.id_sub
+WHERE subscription.price > 42;
